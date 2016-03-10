92b6c7656c5ed2e2542dc227d056c0c2c399a1f8
mario <mrsitofull@gmail.com>
2016-03-10 20:20:05 +0100

    provando

--- src/main/java/mario/Controller.java
+++ src/main/java/mario/Controller.java
@@ -38,6 +38,8 @@ public class Controller {
     @FXML
     private ListView listcapitulos;
     private ObservableList<String> items = FXCollections.observableArrayList ();
+    private ObservableList<String> options = FXCollections.observableArrayList();
+    WebDriver driver;
     Alert alert = new Alert(Alert.AlertType.WARNING);
     ArrayList<String> elements= new ArrayList<String>();
     @FXML
@@ -46,26 +48,41 @@ public class Controller {
     }
 
     public void conectarPagina(){
-        WebDriver driver = new FirefoxDriver();
+        driver = new FirefoxDriver();
         driver.navigate().to("http://seriesblanco.com");
 
         WebElement we= driver.findElement(By.id("buscar-blanco"));
         we.sendKeys(textBuscar.getText());
         we.submit();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.findElement(By.xpath("//div[@id='Blog1']/div[1]/div[1]/div/div[2]/div[1]/div/a")).click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //driver.findElements(By.xpath("//div[@id='post-body-8452518480913717526']/h2")).size();
-        for (int i = 0;i<driver.findElements(By.xpath("//div[@id='post-body-8452518480913717526']/h2")).size();i++){
-            System.out.println(driver.findElements(By.xpath("//div[@id='post-body-8452518480913717526']/h2[(i+1)]")).get(i));
-            elements.add("Temporada"+(i+1));
-        }
+        conteoTemporadas();
+
+       /* for (int i = 0;i<driver.findElements(By.xpath("//div[@id='post-body-8452518480913717526']/h2")).size();i++){
+            //System.out.println(driver.findElements(By.xpath("//div[@id='post-body-8452518480913717526']/h2")).get(i));
+            //options.add("Temporada "+(i+1));
+            combotemporada.getItems().add("Temporada " +(i+1));
+        }*/
+
+        //combotemporada = new ComboBox();
+        //combotemporada.setItems(options);
         //driver.findElement(By.id("buscar-blamco")).submit();
         //driver.findElement(By.id("loginbtn")).click();
         //driver.findElement(By.linkText("MÒDUL 3: PROGRAMACIÓ 2")).click();
         //System.out.println(driver.findElements(By.partialLinkText("Tasca")).size()+1+"chocolata");
 
         //$x("//div[contains(@id,'Blog1')]/div[1]/div[1]/div[2]/")
+            WebElement img = driver.findElement(By.id("port_serie"));
+            String url= img.getAttribute("src");
+            System.out.println(url);
+
+    }
+    private void conteoTemporadas(){
+        for (int i = 0;i<driver.findElements(By.xpath("//div[@id='post-body-8452518480913717526']/h2")).size();i++){
+            combotemporada.getItems().add("Temporada " +(i+1));
+        }
     }
 
 
